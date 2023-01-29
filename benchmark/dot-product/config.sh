36a86e800e1dcb2fef0cf4af6f1195aa110aa16c
ckf104 <2414781665@qq.com>
2023-01-29 04:29:11 -0800

    add benchmark

---
+++ benchmark/dot-product/config.sh
@@ -0,0 +1,2 @@
+cmake -DCMAKE_CXX_COMPILER=/home/ckf/tmp/llvm-12.0.0-x86/bin/clang++ -DCMAKE_BUILD_TYPE=release -B build
+cmake -DCMAKE_CXX_COMPILER=/home/ckf/tmp/rvv/vegen_origin/build/vegen-clang++  -DCMAKE_BUILD_TYPE=release -B build
